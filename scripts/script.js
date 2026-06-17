613c0ba6844b1162915d2a01371c982bbe8a96fa
MaxL007 <Maxfireblue47@gmail.com>
2026-06-17 00:02:31 -0400

    Atualização

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,18 @@ nome.addEventListener("mouseover", function(){
 nome.addEventListener("mouseout", function(){
     nome.innerHTML="...";
 });
+document.getElementById("formContato").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const nome = document.getElementById("nomeInput").value;
+    const mensagem = document.getElementById("mensagem").value;
+    const origem = document.getElementById("origem").value;
+
+    if(nome === "" || mensagem === "" || origem === ""){
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    document.getElementById("msgFinal").innerText =
+        "✅ Formulário enviado com sucesso!";
+});
